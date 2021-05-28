d943c74d18ab5b807edacc1ffc03586a1e7b9379
jung-m <73668285+jung-m@users.noreply.github.com>
2021-05-28 15:09:57 +0200

    Added feature to draw glider instead of cell.

---
+++ Glider.js
@@ -0,0 +1,34 @@
+function Glider(x, y, squaresArray) {
+  this.leftX = x;
+  this.leftY = y;
+
+  this.cells = [];
+
+  let k;
+  for (k = x; k <= x + 2; k++) {
+    this.cells.push(squaresArray[k][y]);
+  }
+  this.cells.push(squaresArray[k - 1][y - 1]);
+  this.cells.push(squaresArray[k - 2][y - 2]);
+
+  this.display = function () {
+    this.cells.forEach((e) => {
+      e.setAlive();
+      e.display();
+    });
+  };
+
+  this.clicked = function () {};
+
+  this.setAlive = function () {
+    this.cells.forEach((e) => {
+      e.setAlive();
+    });
+  };
+
+  this.kill = function () {
+    this.cells.forEach((e) => {
+      e.kill();
+    });
+  };
+}
