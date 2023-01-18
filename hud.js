6eb45b3787955f15be6c2e5fad51c3f59dfbb88e
jung-m <markiyoung97@gmail.com>
2023-01-18 16:46:13 +0100

    Added icons for hud.

--- hud.js
+++ hud.js
@@ -10,36 +10,56 @@ function initHud() {
     overlay = createElement("overlay");
     overlay.class("overlay");
 
-    startButton = createButton("Start");
-    startButton.class("baseButton defaultButton");
+    startButton = createButton("");
+    startButton.class("baseButton defaultButton playPauseButton");
     startButton.parent(overlay);
     startButton.mousePressed(resumeGame);
     buttons.push(startButton);
 
-    pauseButton = createButton("Pause");
-    pauseButton.class("baseButton defaultButton");
+    playIcon = createImg("./images/buttons/play.png", "");
+    playIcon.class("buttonIcon");
+    playIcon.parent(startButton);
+
+    pauseButton = createButton("");
+    pauseButton.class("baseButton defaultButton playPauseButton");
     pauseButton.parent(overlay);
     pauseButton.mousePressed(pauseGame);
     buttons.push(pauseButton);
     pauseButton.hide();
 
-    clearButton = createButton("Clear");
-    clearButton.class("baseButton defaultButton");
+    pauseIcon = createImg("./images/buttons/pause.png", "");
+    pauseIcon.class("buttonIcon");
+    pauseIcon.parent(pauseButton);
+
+    clearButton = createButton("");
+    clearButton.class("baseButton defaultButton playPauseButton");
     clearButton.parent(overlay);
     clearButton.mousePressed(resetGame);
     buttons.push(clearButton);
 
-    gliderDrawButton = createButton("Glider");
+    clearIcon = createImg("./images/buttons/clear.png", "");
+    clearIcon.class("buttonIcon");
+    clearIcon.parent(clearButton);
+
+    gliderDrawButton = createButton("");
     gliderDrawButton.class("baseButton defaultButton");
     gliderDrawButton.parent(overlay);
     gliderDrawButton.mousePressed(toggleGliderDraw);
     buttons.push(gliderDrawButton);
 
-    heavyGliderDrawButton = createButton("Heavy Glider");
+    gliderIcon = createImg("./images/buttons/glider.png", "");
+    gliderIcon.class("buttonIcon");
+    gliderIcon.parent(gliderDrawButton);
+
+    heavyGliderDrawButton = createButton("");
     heavyGliderDrawButton.class("baseButton defaultButton");
     heavyGliderDrawButton.parent(overlay);
     heavyGliderDrawButton.mousePressed(toggleHeavyGliderDraw);
     buttons.push(heavyGliderDrawButton);
+
+    heavyGliderIcon = createImg("./images/buttons/heavyGlider.png", "");
+    heavyGliderIcon.class("buttonIcon");
+    heavyGliderIcon.parent(heavyGliderDrawButton);
 }
 
 function activateButton(button) {
